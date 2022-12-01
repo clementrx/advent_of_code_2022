3e05acc0d7b9e7e2d39c3af36db589608181ab00
Clément Rieux <33658640+clementrx@users.noreply.github.com>
2022-12-01 16:11:39 +0100

    Add files via upload

---
+++ day1/day1/day01.R
@@ -0,0 +1,20 @@
+library(here)
+
+input <- readLines(here('day1',"input01.txt"))
+
+w <- c(0,which(input==""))
+
+elves <- list()
+
+for(i in seq_along(w)){
+  z <- ifelse(i==length(w),length(input),w[i+1]-1)
+  elves[[i]]<-as.integer(input[(w[i]+1):z])
+}
+
+calories <- sapply(elves,sum)
+
+max(calories)
+# 70720
+
+sum(tail(sort(calories),3))
+# 207148
